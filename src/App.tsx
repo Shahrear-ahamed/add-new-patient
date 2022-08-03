b3cd86107d6a41ad247c243592cd9a6d6db8a215
Shahrear ahamed <its.shahrear@gmail.com>
2022-08-03 17:14:00 +0600

    make some vadidation and find error

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import './global.css'
 import AddNewPatient from "./Components/AddNewPatient";
 import PatientInfo from "./Components/PatientInfo";
 
 function App() {
   return (
-    <>
+    <div className="wrapper">
         <AddNewPatient />
         <PatientInfo />
-    </>
+    </div>
   );
 }
 
--- src/Components/PatientInfo.tsx
+++ src/Components/PatientInfo.tsx
@@ -1,45 +1,49 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import {Field, Form, Formik} from 'formik';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import {Button,Stack} from "@mui/material";
+import * as Yup from  "yup";
 
+
+
+interface addNewPatientDetails {
+    name: string;
+    age:string;
+}
+const ValidationSchema = Yup.object().shape({
+    name: Yup.string().required("Name is require")
+})
 const PatientInfo = () => {
 
-    const formik = useFormik({
-        initialValues: {
-            firstName: '',
-            lastName: '',
-            name: '',
-            age:''
-        },
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
-        },
-    });
+    const initialValues: addNewPatientDetails = { name: '', age:"" };
 
     return (
-        <section>
-            <form onSubmit={formik.handleSubmit}>
-                {/*patient details are here*/}
-                <div>
-                    <h4>Patient Details</h4>
-
-                    <div>
-                        <label htmlFor="name" className="important-field">Name</label>
-                        <input
-                            id="name"
-                            name="name"
-                            type="text"
-                            onChange={formik.handleChange}
-                            value={formik.values.name}
-                        />
-                        <div>
-                            <label htmlFor="age" className="important-field">Date of Birth of Age</label>
-                            <input id="age" name="age" type="text" placeholder="DD/MM/YYYY or Age in Years" onChange={formik.handleChange} value={formik.values.age}/>
-                        </div>
+        <section className="details-div">
+            <Formik
+                initialValues={initialValues}
+                validationSchema={ValidationSchema}
+                onSubmit={(values, actions) => {
+                    console.log(values);
+                    actions.setSubmitting(false);
+                }}
+            >
+                <Form>
+                    <label htmlFor="name" className="important-field">First Name</label>
+                    <Field id="name" name="name" placeholder="Enter Name" />
+                    <button type="submit">Submit</button>
+                    <div className="buttons-div">
+                        <Stack direction="row" spacing={2}>
+                            <Button type="submit"  variant="outlined" startIcon={<DeleteIcon />}>
+                                Cancel
+                            </Button>
+                            <Button type="submit" variant="contained" color="success" endIcon={<SendIcon />}>
+                                Submit
+                            </Button>
+                        </Stack>
                     </div>
-                </div>
-
-                <button type="submit">Submit</button>
-            </form>
+                </Form>
+            </Formik>
         </section>
     );
 };
